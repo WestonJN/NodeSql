e5c90ef8e16ab899b91dea82a20729b3ecc2efd0
WestonJN <westonnyirenda@gmail.com>
2020-02-04 11:21:56 +0200

    function edits

--- src/script.js
+++ src/script.js
@@ -11,49 +11,13 @@ const addNewVisitor = (visitorname,visitorage,dateofvisit,timeofvisit,assistant,
   
     const sql='INSERT INTO visitors(visitorname,visitorage,dateofvisit,timeofvisit,assistant,comments) VALUES ($1, $2, $3, $4, $5, $6)';
     const values= 
-      // [visitorname,visitorage,dateofvisit,timeofvisit,assistant,comments];
-      // ['Weston','23','01/01/2020','12:05','George','Its ayt']
-      ['Bob','24','01/02/2020','13:10','Sam','Basic']
-    
+      [visitorname,visitorage,dateofvisit,timeofvisit,assistant,comments];
+      
     pool.query(sql, values, (err,res)=>{
       if (err) throw err;
       console.log("Number of records inserted: " + res.rowCount);
     pool.end();
     });
   }
-
-
-// helloWorld();
-
-
-// const addNewVisitor = () => {
-//   pool.query(
-//     "SELECT $1::text as message",
-//     ["Hello world!"],
-//     (error, results) => {
-//       if (error) {
-//         throw error;
-//       }
-
-//       console.log(results.rows);
-//     }
-//   );
-// };
-
-// helloWorld();
-
-  // con.connect(function(err){
-  //   if (err) throw err;
-  //   const sql="INSERT INTO visitors(visitorname,visitorage,dateofvisit,timeofvisit,assistant,comments VALUES ?)";
-  //   const values= [
-  //     ['Weston','23','01/01/2020','12:05','George','Its ayt'],
-  //     ['Bob','24','01/02/2020','13:10','Sam','Basic']
-  //   ];
-  //   con.query(sql, [values],function (err,result){
-  //     if (err) throw err;
-  //     console.log("Number of records inserted: " + result.affectedRows);
-  //   });
-  // });
-
 
 addNewVisitor();
