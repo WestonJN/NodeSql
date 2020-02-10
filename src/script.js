fee703db9fc29da37ec4686f9b587c7f81753ed2
WestonJN <westonnyirenda@gmail.com>
2020-02-10 11:53:03 +0200

    script changes

--- src/script.js
+++ src/script.js
@@ -1,96 +1,109 @@
 require("dotenv").config();
-// const db=require('db')
-// db.connect({
-//   host:process.env.DB_HOST,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASS
-// })
-const Pool = require("pg").Pool;
-const pool = new Pool({
+
+const Client = require("pg").Client;
+const client = new Client({
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASS,
 });
 
-const addNewVisitor = (visitorname,visitorage,dateofvisit,timeofvisit,assistant,comments) => {
+client.connect()
+const addNewVisitor = async(visitorname,visitorage,dateofvisit,timeofvisit,assistant,comments) => {
   
     const sql='INSERT INTO visitors(visitorname,visitorage,dateofvisit,timeofvisit,assistant,comments) VALUES ($1, $2, $3, $4, $5, $6)';
     const values= 
       [visitorname,visitorage,dateofvisit,timeofvisit,assistant,comments];
       
-    pool.query(sql, values, (err,res)=>{
-      if (err) throw err;
-      console.log("Number of records inserted: " + res.rowCount);
+    // client.query(sql, values, (err,res)=>{
+    //   if (err) throw err;
+    //   console.log("Number of records inserted: " + res.rowCount);
 
-    });
+    // });
+
+	try {
+		query = await client.query(sql,values)
+		console.log(query.rows)
+		// await client.end()
+		return query.rows
+	} catch(e) {
+		console.log(e);
+		// await client.end()
+	}
   }
 
-  const listVisitors = () => {
+  const listVisitors = async() => {
   
     const sql='SELECT * FROM visitors';
-    
-    pool.query(sql, (err,res)=>{
-      if (err) throw err;
-      console.log("Number of records listed: " + res.rowCount);
-    
-    });
+    try {
+      query = await client.query(sql)
+      console.log(query.rows)
+      // await client.end()
+      return query.rows
+    } catch(e) {
+      console.log(e);
+      // await client.end()
+    }
   }
 
-  const deleteVisitor = (visitorid) => {
+  const deleteVisitor = async(visitorid) => {
   
     const sql='DELETE FROM visitors WHERE visitorid=$1';
     const values= 
       [visitorid];
       
-    pool.query(sql, values, (err,res)=>{
-      if (err) throw err;
-      console.log("Number of records deleted: " + res.rowCount);
-    
-    });
+      try {
+        query = await client.query(sql,values)
+        console.log(query.rows)
+        await client.end()
+        return query.rows
+      } catch(e) {
+        console.log(e);
+        await client.end()
+      }
   }
 
-  const updateVisitor = (visitorid,visitorname,visitorage,dateofvisit,timeofvisit,assistant,comments) => {
+  const updateVisitor = async(visitorid,visitorname,visitorage,dateofvisit,timeofvisit,assistant,comments) => {
   
     const sql='UPDATE visitors SET visitorname=$2,visitorage=$3,dateofvisit=$4,timeofvisit=$5,assistant=$6,comments=$7 WHERE visitorid =$1';
     const values= 
       [visitorid,visitorname,visitorage,dateofvisit,timeofvisit,assistant,comments];
     
       
-    pool.query(sql, values, (err,res)=>{
+    client.query(sql, values, (err,res)=>{
       if (err) throw err;
       console.log("Number of records updates: " + res.rowCount);
   
     });
   }
 
-  const viewVisitor = (visitorid) => {
+  const viewVisitor = async(visitorid) => {
   
     const sql='SELECT * FROM visitors WHERE visitorid = $1';
     const values= [visitorid]
-    pool.query(sql,values, (err,res)=>{
+    client.query(sql,values, (err,res)=>{
       if (err) throw err;
       console.log("Number of records displayed: " + res.rowCount);
     
     });
   }
-  const deleteAllVisitor = () => {
+  const deleteAllVisitor = async() => {
   
     const sql='DELETE FROM visitors';
       
-    pool.query(sql, (err,res)=>{
+    client.query(sql, (err,res)=>{
       if (err) throw err;
       console.log("Number of records deleted: " + res.rowCount);
-    pool.end();
+    client.end();
     });
   }
 
 addNewVisitor();
 listVisitors();
 deleteVisitor();
-updateVisitor();
-viewVisitor();
-deleteAllVisitor();
+// updateVisitor();
+// viewVisitor();
+// deleteAllVisitor();
 
-// module.exports ={ 
-//   addNewVisitor
-// }
+module.exports ={ 
+  addNewVisitor
+}
